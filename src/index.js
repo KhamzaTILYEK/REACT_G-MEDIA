b88dd6e6c02e16610bf15232148e91687385eb23
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-01-17 17:40:04 +0800

    router
    npm react router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./Pages/Home";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <div className="bg-[#f72] ">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Link to="/home">
+              <h1>Home Page</h1>
+            </Link>
+          }
+        />
+        <Route exact path="home" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
