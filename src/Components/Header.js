c02e5c508d9b65c611288aac5ca23008665546c3
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-01-31 09:39:39 +0800

    Logo component added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Button from "./Button";
+import Logo from "./Logo";
 
 export default function Header() {
   const [a, setA] = useState(false);
   return (
     <div>
+      <Logo />
       <div className={a ? "text-[#f32]" : "text-[#f72]"}>Header</div>
       <Button
         type={a ? 1 : 0}
---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Logo({ name, size }) {
+  return (
+    <div
+      className={`font-[700] text-[#f72] p-2 font${size ? size : "-[40px]"}`}
+    >
+      {name ? name : "G-MEDIA"}
+    </div>
+  );
+}
