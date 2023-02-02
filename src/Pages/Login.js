b6d0f0bf0a13ee6df6ec1984e41522bd19e94fc7
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-02-02 11:32:38 +0800

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Login() {
-  return <div className="bg-[#f72] w-full h-full"></div>;
+  return (
+    <div className=" bg-gradient-to-r from-[#F62] to-[#f82] absolute top-0 left-0 w-full h-full flex justify-center items-center">
+      <div className="border w-[400px] h-[200px] backdrop-blur-[2px] p-4 rounded-xl  ">
+        d fddfv fd d
+      </div>
+    </div>
+  );
 }
