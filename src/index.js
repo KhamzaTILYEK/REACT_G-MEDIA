859fff7c38108d721ab8460ae0b67c5f4abafe64
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-02-02 08:58:36 +0800

    Login page route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import Home from "./Pages/Home";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -14,11 +15,17 @@ root.render(
           exact
           path="/"
           element={
-            <Link to="/home">
-              <h1>Home Page</h1>
-            </Link>
+            <>
+              <Link to="/home">
+                <h1>Home Page</h1>
+              </Link>
+              <Link to="/login">
+                <h1>Login</h1>
+              </Link>
+            </>
           }
         />
+        <Route exact path="login" element={<Login />} />
         <Route exact path="home" element={<Home />} />
       </Routes>
     </Router>
