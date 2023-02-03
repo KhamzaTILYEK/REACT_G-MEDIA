cd821dacfbacc7dcff52866a27ad8ec21ec7ad14
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-02-03 09:00:02 +0800

    Components

---
+++ src/Pages/Components.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../Components/Button";
+import Input from "../Components/Input";
+import Logo from "../Components/Logo";
+
+export default function Components() {
+  return (
+    <div>
+      <Input />
+      <Button />
+      <Logo />
+    </div>
+  );
+}
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Login() {
   return (
     <div className=" bg-gradient-to-r from-[#F62] to-[#f82] absolute top-0 left-0 w-full h-full flex justify-center items-center">
-      <div className="border  backdrop-blur-[2px] p-4 rounded-xl  ">
-        d fddfv fd d
+      <div className="border  backdrop-blur-[2px] p-4 rounded-xl flex">
+        <div></div>
       </div>
     </div>
   );
