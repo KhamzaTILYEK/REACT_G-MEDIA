97ecbc0e32c946b5e0973482f21bc237b77648d6
Khamza Tilyek <tilekkhamza9702@gmail.com>
2023-02-03 09:12:39 +0800

    Update Input.js

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Input() {
-  return <input className="" />;
+export default function Input({ placeholder, disabled }) {
+  return (
+    <input
+      disabled={disabled}
+      className="border border-[#aaa] rounded-full px-3 py-1  focus:outline-none focus:border-[#f72] text-[#000]"
+      placeholder={placeholder ? placeholder : "Input"}
+    />
+  );
 }
